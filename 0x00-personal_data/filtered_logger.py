b1aab5a4b55b9ec35462adde4126d502cc6e4b61
MegaChie <workspace.mega@gmail.com>
2024-07-04 00:07:01 -0700

    Redact with a class

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """Task 0"""
+import logging
 import re
 from typing import List
 
@@ -11,3 +12,26 @@ def filter_datum(fields: List[str], redaction: str,
         message = re.sub(field+"=.*?"+separator,
                          field+"="+redaction+separator, message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """Redacting Formatter class"""
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """Initializes the class object"""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        Filter values in incoming log records
+        using the filter_datum function.
+        """
+        message = super(RedactingFormatter, self).format(record)
+        redacted = filter_datum(self.fields, self.REDACTION,
+                                message, self.SEPARATOR)
+        return redacted
