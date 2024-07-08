51aa849bedd2f7a837f0bb771a61aa8a98f6ab00
MegaChie <workspace.mega@gmail.com>
2024-07-08 16:43:01 -0700

    Adds class and set app.py to user it

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -3,6 +3,7 @@
 Route module for the API
 """
 from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
@@ -15,7 +16,9 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 Auth_Type = os.getenv("AUTH_TYPE")
-if Auth_Type:
+if Auth_Type == "basic_auth":
+    auth = BasicAuth()
+else:
     auth = Auth()
 
 
