60707610c353e6ae027380273bf4877f97c39494
MegaChie <workspace.mega@gmail.com>
2024-07-09 06:25:09 -0700

    Decodes the Base64 part in header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -10,7 +10,7 @@ class BasicAuth(Auth):
                                             authorization_header: str) -> str:
         """
         Returns the Base64 part of the Authorization header
-        for a Basic Authentication
+        for a Basic Authentication.
         """
         if not authorization_header:
             return None
@@ -20,3 +20,21 @@ class BasicAuth(Auth):
             return None
         authorization_header = authorization_header[6:]
         return authorization_header
+
+    def decode_base64_authorization_header(self,
+                                    base64_authorization_header: str) -> str:
+        """
+        Returns the decoded value
+        of a Base64 string base64_authorization_header.
+        """
+        header = base64_authorization_header
+        if not header:
+            return None
+        if not isinstance(header, str):
+            return None
+        try:
+            header_Bytes = base64.b64decode(header, validate=True)
+            UTF8_header = header_Bytes.decode("utf-8")
+            return UTF8_header
+        except (base64.binascii.Error, UnicodeDecodeError):
+            pass
