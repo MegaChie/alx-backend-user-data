367535e80d85b1f14c6f53ce2d731edb42954c2d
MegaChie <workspace.mega@gmail.com>
2024-07-16 16:49:45 -0700

    Adds hashing for users

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 """Authentication for the app"""
 import bcrypt
+from db import DB
+from sqlalchemy.orm.exc import NoResultFound
+from user import User
 
 
 def _hash_password(password: str) -> bytes:
@@ -10,3 +13,20 @@ def _hash_password(password: str) -> bytes:
     salting = bcrypt.gensalt()
     hashed_paswrd = bcrypt.hashpw(password.encode('utf-8'), salting)
     return hashed_paswrd
+
+
+class Auth:
+    """Auth class to interact with the authentication database"""
+    def __init__(self):
+        """Initializes the class"""
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Hashes the password of the new user"""
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            hased_Pasword = _hash_password(password)
+            new_User = self._db.add_user(email, hased_Pasword)
+            return new_User
+        raise ValueError("User {} already exists".format(email))
