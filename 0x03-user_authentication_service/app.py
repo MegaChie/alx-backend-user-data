af5b4a200ab94d88f4d9bbb8cde34a9f555fe064
MegaChie <workspace.mega@gmail.com>
2024-07-16 17:16:35 -0700

    revert

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -13,17 +13,5 @@ def greeting() -> str:
     return jsonify({"message": "Bienvenue"}), 200
 
 
-@app.route("/users", methods=["POST"], strict_slashes=False)
-def users() -> str:
-    """Register new users"""
-    email = request.form.get("email")
-    paswrd = request.form.get("password")
-    try:
-        AUTH.register_user(email, paswrd)
-        return jsonify({"email": email, "message": "user created"}), 200
-    except Exception:
-        return jsonify({"message": "email already registered"}), 400
-
-
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
