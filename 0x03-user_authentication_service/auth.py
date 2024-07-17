897afb34a1ac917006eb0ab1b69ba4d175b4352f
MegaChie <workspace.mega@gmail.com>
2024-07-16 17:43:26 -0700

    Checks for login credentials

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -30,3 +30,13 @@ class Auth:
             new_User = self._db.add_user(email, hased_Pasword)
             return new_User
         raise ValueError("User {} already exists".format(email))
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Returns True is user is logged in, False is not"""
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return False
+        paswrd = user.hashed_password
+        check_Paswrd = password.encode("utf-8")
+        return bcrypt.checkpw(check_Paswrd, paswrd)
