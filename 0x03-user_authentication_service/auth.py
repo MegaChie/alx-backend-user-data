c884baa980e7b93ef1fe75f12149a4680bdce14b
MegaChie <workspace.mega@gmail.com>
2024-07-18 12:34:09 -0700

    Change logic

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -76,12 +76,13 @@ class Auth:
 
     def get_reset_password_token(self, email: str) -> str:
         """Generates a reset token for the user and returns it"""
-        found = self._db.find_user_by(email=email)
-        if not found:
+        try:
+            found = self._db.find_user_by(email=email)
+            token = _generate_uuid()
+            self._db.update_user(user.id, reset_token=token)
+            return token
+        except NoResultFound:
             raise ValueError
-        token = _generate_uuid()
-        self._db.update_user(user.id, reset_token=token)
-        return token
 
     def update_password(self, reset_token: str, password: str) -> None:
         """Updates the password of a user based on the reset token"""
